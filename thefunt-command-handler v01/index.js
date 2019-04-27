36dd83aae692d80b16877ce07f67a1da3a0605c7
Aktila <49373230+AktilaCengiz@users.noreply.github.com>
2019-04-27 16:27:39 +0300

    Delete index.js

--- thefunt-command-handler v01/index.js
+++
@@ -1,8 +0,0 @@
-const package = require("./package.json");
-
-module.exports = {
-    Client: require('./src/client.js'),
-    AdvancedClient: require('./src/client.js'),
-    version: package.version,
-    aciklama: package.description
-};
