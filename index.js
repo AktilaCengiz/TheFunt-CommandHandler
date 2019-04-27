86ab75b98bb4fc90040a13cad1bacae6485efad4
Aktila <49373230+AktilaCengiz@users.noreply.github.com>
2019-04-27 16:30:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const package = require("./package.json");
 
 module.exports = {
     Client: require('./src/client.js'),
-    AdvancedClient: require('./src/client.js'),
+    TheFuntClient: require('./src/client.js'),
     version: package.version,
     aciklama: package.description
 };
