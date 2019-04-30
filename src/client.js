6e89fee9a41b1418c021b71d30702a82f3bef914
silberpfeilv2 <40671874+silberpfeilv2@users.noreply.github.com>
2019-04-30 21:25:39 +0300

    Update client.js

--- src/client.js
+++ src/client.js
@@ -364,7 +364,7 @@ class TheFuntClient extends Discord.Client {
                     }
 
                 if (cxc[i+1] === undefined) {
-                    msg.channel.send(new Discord.RichEmbed().setColor("#9b59b6").setDescription(cmd.args[i].soru).setFooter("<loading2:503562521077219328> | Komut isteğini iptal etmek için \"iptal\" yazınız. \n Komut isteği otomatik olarak \"30\" saniye sonra iptal edilecektir."))
+                    msg.channel.send(new Discord.RichEmbed().setColor("#9b59b6").setDescription(cmd.args[i].soru).setFooter(client.emojis.get("503562521077219328")+" | Komut isteğini iptal etmek için \"iptal\" yazınız. \n Komut isteği otomatik olarak \"30\" saniye sonra iptal edilecektir."))
                     
                     try {
 
@@ -486,7 +486,7 @@ class TheFuntClient extends Discord.Client {
                 return
             };
 
-        msg.channel.send(new Discord.RichEmbed().setColor("#9b59b6").setDescription(cmd.args[i].soru).setFooter("<loading2:503562521077219328> | Komut isteğini iptal etmek için \"iptal\" yazınız. \n Komut isteği otomatik olarak \"30\" saniye sonra iptal edilecektir."))
+        msg.channel.send(new Discord.RichEmbed().setColor("#9b59b6").setDescription(cmd.args[i].soru).setFooter("client.emojis.get("503562521077219328") | Komut isteğini iptal etmek için \"iptal\" yazınız. \n Komut isteği otomatik olarak \"30\" saniye sonra iptal edilecektir."))
 
         try {
 
